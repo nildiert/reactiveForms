8f587b0db1c89275374ff73e57b7bdb74a70108c
nildiert23 <nildiert.jimenez@hrinteractive.co>
2019-01-14 13:53:14 -0500

    Primer formulario reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  user: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+ngOnInit() {
+  this.user = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    password: ['', Validators.required],
+    passwordRepeat: ['', Validators.required]
+  });
 }
+
+onSubmit() {
+  console.log(this.user);
+}
+
+
+}
+
+
+
