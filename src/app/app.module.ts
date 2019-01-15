56841fc5f9396250697193cff98dfe440befc180
nildiert23 <nildiert.jimenez@hrinteractive.co>
2019-01-15 11:07:18 -0500

    Invest input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ListComponent } from './formulario/list/list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
